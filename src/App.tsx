c00fcf24503f91adfcfb2f56ab63bf2fe1affe3f
Agustin Martinez <agustinmartinezyt@gmail.com>
2025-10-23 20:17:46 -0300

    feat: implement main App component with Card integration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Card } from "./components/card"
+
+function App() {
+  return (
+    <div className="w-full mx-auto min-h-screen flex items-center justify-center bg-yellow-400">
+      <Card/>
+    </div>
+  )
+}
+
+export default App
